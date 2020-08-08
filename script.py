df5a0f7d0248a0ef05eba8e35fe28a8e9ecbd948
ktsotras <kevin.tsotras@gmail.com>
2020-08-08 10:46:57 -0400

    score dictionary completed

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
+letters_to_points = { key:value for key, value in zip(letters, points)}
+letters_to_points[' '] = 0
+
+
