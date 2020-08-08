1e9853cd2b00355ab362cdb269038ca506f8cb41
ktsotras <kevin.tsotras@gmail.com>
2020-08-08 11:11:20 -0400

    added play_word function

--- script.py
+++ script.py
@@ -12,6 +12,9 @@ def score_word(word):
 
     return point_total
 
+def play_word(player, word):
+    player_to_words[player].append(word)
+
 player_to_words = {'player1':['BLUE', 'TENNIS', 'EXIT'], 'wordNerd':['EARTH', 'EYES', 'MACHINE'], 'Lexi Con':['ERASER', 'BELLY', 'HUSKY'], 'Prof Reader':['ZAP', 'COMA', 'PERIOD']}
 
 player_to_points = {}
@@ -23,5 +26,7 @@ for player, words in player_to_words.items():
         player_points += score_word(word)
 
     player_to_points[player] = player_points
+
+
 
 print(player_to_points)
